d43eddf4ac21aa656505d45ecc25f5de9a1e8515
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-22 12:47:30 +0000

    Update premium.js

--- src/pages/premium.js
+++ src/pages/premium.js
@@ -13,7 +13,7 @@ export default function Premium() {
       animate={{ opacity: 1 }}
     >
       <Head>
-        <title>Premium | Musi</title>
+        <title>Premium | Codex</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
