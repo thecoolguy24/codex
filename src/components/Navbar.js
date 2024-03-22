5bfae8daff280e83cefaa6c1a859dc023d00e753
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-22 13:34:16 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
           <ul>
             <li>
               <Link className="h-color" href="/">
-                Musi
+                Codex
               </Link>
             </li>
           </ul>
