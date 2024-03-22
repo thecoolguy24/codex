e22e7667f0c89a1a6d6431786de9a7b1c668ae96
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-22 13:00:58 +0000

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -12,7 +12,7 @@ const Showcase = () => {
       <main className="showcase">
         <h1 className="showcase-header h-color">Codex</h1>
         <h2 className="showcase-header-text h-color">
-          The only discord bot youl ever need!
+          The only discord bot you'll ever need!
         </h2>
         <p className="showcase-para p-color">
           One of the awesome discord bot with tons of commands that will fill
@@ -21,16 +21,12 @@ const Showcase = () => {
 
         <span className="p-color"></span>
         <div className="showcase-btn">
-          <button className="btn">
-            <Link className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1200772498942345297&permissions=8&scope=bot">
-              Invite now
-            </Link>
-          </button>
-
-          <button className="btn">
-            <Link className="h-color no-decoration" href="https://discord.gg/eJsvNMFG">
-              Join the support server
-            </Link>
+          <Link href="https://discord.com/oauth2/authorize?client_id=1200772498942345297&permissions=8&scope=bot">
+            <a className="btn h-color no-decoration">Invite now</a>
+          </Link>
+          <Link href="https://discord.gg/eJsvNMFG">
+            <a className="btn h-color no-decoration">Join the support server</a>
+          </Link>
         </div>
       </main>
     </motion.div>
