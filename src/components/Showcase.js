7e0ac1767cfbea8bb70a6f34c6963945a242dae8
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-22 12:46:11 +0000

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -10,9 +10,9 @@ const Showcase = () => {
       animate={{ opacity: 1 }}
     >
       <main className="showcase">
-        <h1 className="showcase-header h-color">Musi</h1>
+        <h1 className="showcase-header h-color">Codex</h1>
         <h2 className="showcase-header-text h-color">
-          The only discord music bot youl ever need!
+          The only discord bot youl ever need!
         </h2>
         <p className="showcase-para p-color">
           One of the awesome discord bot with tons of commands that will fill
@@ -22,26 +22,15 @@ const Showcase = () => {
         <span className="p-color"></span>
         <div className="showcase-btn">
           <button className="btn">
-            <Link className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1137417865016918149&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fmusi.lol%2Fadded&scope=bot+identify">
+            <Link className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1200772498942345297&permissions=8&scope=bot">
               Invite now
             </Link>
           </button>
 
           <button className="btn">
-            <Link className="h-color no-decoration" href="https://discord.gg/YNTTT2EPqC">
+            <Link className="h-color no-decoration" href="https://discord.gg/eJsvNMFG">
               Join the support server
             </Link>
-          </button>
-          <button className="btn">
-            <Link className="h-color no-decoration" href="https://top.gg/bot/1137417865016918149/vote">
-              Vote for Musi
-            </Link>
-          </button>
-                      <button className="btn">
-            <Link className="h-color no-decoration" href="/premium">
-              Premium
-            </Link>
-          </button>
         </div>
       </main>
     </motion.div>
