e170f40fa6396a3d3e444a1536d20b42a028be84
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-22 13:43:33 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
               </Link>
               </li>
               <li>
-              <Link className="p-color" href="https://discord.com/oauth2/authorize?client_id=1200772498942345297&permissions=8&scope=bot">
+              <Link className="p-color" href="https://discord.com/oauth2/authorize?client_id=1200772498942345297&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fcodexbot.lol%2Fadded&scope=bot+identify">
           Add
               </Link>
                   </li>
